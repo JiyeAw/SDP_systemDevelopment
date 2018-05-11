c5f0b1a44044cc82c7a30a6e77d2b67582ea68e3
JiYe Aw <jiye_aw96@live.com>
2018-05-11 12:21:10 +1000

    prototype version of the data inporting

--- src/Item/TemperatureControlled.java
+++ src/Item/TemperatureControlled.java
@@ -4,7 +4,7 @@ public class TemperatureControlled extends NormalItem {
 
 	private int temperature;
 	
-	public TemperatureControlled(String name, double cost, double price, int reorderPoint, int reorderAmount) {
+	public TemperatureControlled(String name, double cost, double price, int reorderPoint, int reorderAmount, int temperature) {
 		super(name, cost, price, reorderPoint, reorderAmount);
 		// TODO Auto-generated constructor stub
 		this.temperature = temperature;
