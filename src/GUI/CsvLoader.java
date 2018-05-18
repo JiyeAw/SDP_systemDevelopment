c22065de1b8a77a16a7096c364cde9cc8b68e296
amitb <amitb@DESKTOP-TL2OJKI>
2018-05-19 01:49:31 +1000

    1.csv loader gui

---
+++ src/GUI/CsvLoader.java
@@ -0,0 +1,62 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.ScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.JFileChooser;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+public class CsvLoader {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CsvLoader window = new CsvLoader();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CsvLoader() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame("Select .CSV file only");
+		frame.setBounds(100, 100, 733, 462);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileSelectionMode(1);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(fileChooser, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 727, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(fileChooser, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 427, Short.MAX_VALUE)
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+}
